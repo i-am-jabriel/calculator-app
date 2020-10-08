bc3e665a12421b54232fc6e9d105e5f75845d561
Abe Johnson <officialjabe@gmail.com>
2020-10-08 19:04:55 -0400

    memory recall feature added and drag added

--- calc.js
+++ calc.js
@@ -4,12 +4,12 @@ var historyScreen = document.querySelector('#history-screen');
 
 var sign = document.querySelector('#sign');
 document.querySelectorAll('button').forEach(x=>x.addEventListener('click',handleClick)  );
-document.addEventListener('keypress',handleKeyPress);
+window.addEventListener('keypress',handleKeyPress);
 var calcText = '';
 var historyText = '';
 var val = 0;
 var signValue = 1;
-var lastOperator='+';
+var lastOperator=null;
 function handleClick(e){
     processInput(e.target.innerHTML);
 }
@@ -22,12 +22,12 @@ function handleKeyPress(e){
     if(key === 8) key = '⌫';
     else if(key === 13) key = '=';
     else if(e.key === 'Delete') key = 'C';
-    else key = String.fromCharCode(key).replace(/\*/,'×').replace(/\//,'÷');
-    if(key.match(/[-\.÷×\+0-9=cCcC]/gi))processInput(key);
+    else key = String.fromCharCode(key).replace(/\*/,'×').replace(/\//,'÷').toLocaleLowerCase();
+    if(key.match(/[-\.÷×\+0-9=cm⌫]/gi))processInput(key);
     else console.log('invalid input ',key);
 }
 function processInput(key){
-    if(isNumber(Number(key))){
+    if(isNumber(Number(key))||key==='m'){
         calcText += key;
     }else{
         switch(key){
@@ -40,8 +40,8 @@ function processInput(key){
                 calcText = calcText.substr(0,calcText.length-1);
                 break;
             case 'C':
-                calcText = '';
-                historyScreen.innerHTML = historyText = '';
+                calcText = historyScreen.innerHTML = historyText = '';
+                lastOperator = null;
                 val = 0;
                 break;
             case 'CE':
@@ -54,7 +54,8 @@ function processInput(key){
                 break;
             case '=':
                 var op = '';
-                if(!historyText.match(/[-÷×\+]/g) )historyText+= lastOperator;
+                if(!lastOperator) historyText = '';
+                if(lastOperator && !historyText.match(/[-÷×\+]/g))historyText+= lastOperator;
                 val = solve(historyText + calcText);
                 calcText = '';
                 historyScreen.innerHTML = historyText = val+'';
@@ -63,6 +64,13 @@ function processInput(key){
                 console.log('vvs');
                 signValue*=-1;
                 sign.innerHTML = signValue===1?'':'-';
+                break;
+            case 'm+':case 'm-':
+                val = solve(historyText + calcText);
+                calcText = '';
+                historyScreen.innerHTML = historyText = val+'';
+                if(key=='m+')memoryRecall += Number(val);
+                else memoryRecall -= Number(val);
                 break;
         }
     }
@@ -73,25 +81,69 @@ function isNumber(value)
     return typeof value === 'number' && isFinite(value);
 }
 function solve(s){
-    return eval(s.replace(/×/g,'*').replace(/÷/g,'/')); 
+    return myEval(s.replace(/×/g,'*').replace(/÷/g,'/').replace(/\m/g,memoryRecall+'')); 
 }
-var _top, _left;
-calculator.style.left = 0;
-calculator.style.top = 0;
-calculator.addEventListener('mousedown', event =>{
-    _left = parseInt(calculator.style.left);
-    _top = parseInt(calculator.style.top);
-    if(!isNumber(top))top=0; 
-    document.addEventListener('mousemove', drag);
-    document.addEventListener('mouseup', endDrag);
-});
+var operators = ['+','-','/','*'];
+function myEval(s){
+    var arr = s.split(/([-\+*\/])/g);
+    console.log(arr);
+    if(arr.length<3)return s;
+    var x = Number(arr[0]);
+    var y = Number(arr[2]);
+    var operator = arr[1];
+
+    switch(operator){
+        case '+':
+            return x+y;
+        case '-':
+            return x-y;
+        case '*':
+            return x*y;
+        case '/':
+            return x/y;
+    }
+    return s;
+}
+
+/* 
+
+    -Memory Recall-
+
+*/
+
+var memoryRecall = 0;
 
 
-var drag = event =>{
-    calculator.style.left = (_left += event.movementX)+'px';
-    calculator.style.top  = (_top += event.movementY)+'px';
+
+/*
+
+    -Dragging Widget-
+
+*/
+
+var dragWidget;
+calculator.addEventListener('mousedown', createDragListenerFor(calculator));
+function createDragListenerFor(widget){
+    return () => {
+        dragWidget = widget;
+        dragWidget.style.position='relative';
+        document.addEventListener('mousemove', drag);
+        document.addEventListener('mouseup', endDrag);
+    }
+}
+
+function getWidgetLeft(){
+    return dragWidget.style.left ? parseFloat(dragWidget.style.left) : 0;
+}
+function getWidgetTop(){
+    return dragWidget.style.top ? parseFloat(dragWidget.style.top) : 0;
+}
+
+function drag(event){
+    dragWidget.style.left = getWidgetLeft() + event.movementX * .75 +'px';
+    dragWidget.style.top  = getWidgetTop()  + event.movementY * .75 +'px';
 };
-var endDrag = event =>{
+function endDrag(event){
     document.removeEventListener('mousemove',drag);
     document.removeEventListener('mouseup', endDrag);
 }
